5e45d2d690ab23cbfb9f8f17a8978884504e54ba
Alvaro R Martin <melkor@aleph.engineering>
2023-10-12 23:13:27 -0400

    #3
    - Implement main Layout
    - SetUp Styles for it

--- src/containers/mainLayout.jsx
+++ src/containers/mainLayout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./containersstyles.css"
+import { Outlet } from 'react-router-dom'
+import NavBar from '../components/navbarComponent/navBar'
 
 const MainLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="main-layout">
+      <NavBar />
+      <div className="content-render">
+        <Outlet />
+      </div>
+    </section>
   )
 }
 
